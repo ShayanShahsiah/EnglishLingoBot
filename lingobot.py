20eb37f9b7dd7d39c2731e313035fc10d8f1fe0e
Xosrov <seyed.alireza.miryazdi@gmail.com>
2020-02-27 14:38:49 +0330

    Pivot

--- lingobot.py
+++ lingobot.py
@@ -44,8 +44,7 @@ class LingoBot:
     @_save_message_to_history
     def _on_command_stop(self, update: Update, context: CallbackContext):
         message: Message = update.message
-        return message.reply_text("This command doesn't do anything yet! But I added this shit\
-            just to test how push and pull work!!")
+        return message.reply_text("This command doesn't do anything yet!")
 
     def _on_callback_query(self, update: Update, context: CallbackContext):
         query: CallbackQuery = update.callback_query
